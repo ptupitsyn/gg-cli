8d420a7cd9e6f4cfa1787a6e24bc4181d2a7357a
Pavel Tupitsyn <ptupitsyn@apache.org>
2026-06-20 11:46:49 +0300

    Improve gg-sql tool

--- gg-sql.cs
+++ gg-sql.cs
@@ -11,4 +11,24 @@ if (args.Length != 1)
 string endpoint = Environment.GetEnvironmentVariable("GG_ENDPOINT") ?? "localhost";
 using var ignite = await IgniteClient.StartAsync(new IgniteClientConfiguration(endpoint));
 
-await ignite.Sql.ExecuteScriptAsync(args[0]);
+await using var rs = await ignite.Sql.ExecuteAsync(null, args[0]);
+
+if (rs.WasApplied)
+{
+    Console.WriteLine("Query executed successfully.");
+}
+
+if (rs.AffectedRows >= 0)
+{
+    Console.WriteLine($"Query executed successfully. Affected rows: {rs.AffectedRows}");
+}
+
+if (rs.HasRowSet)
+{
+    Console.WriteLine(string.Join(" \t ", rs.Metadata.Columns.Select(c => c.Name)));
+
+    await foreach (var row in rs)
+    {
+        Console.WriteLine(string.Join(" \t ", Enumerable.Range(0, rs.Metadata.Columns.Count).Select(i => row[i])));
+    }
+}
